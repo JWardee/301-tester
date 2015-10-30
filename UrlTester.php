09148b96ec30dc7a08bcbdc4b808f916c6338cf6
James Ward <james@impactdesignsuk.net>
2015-10-30 23:24:18 +0000

    +Converted to a class and added basic PHPDoc blocks

---
+++ UrlTester.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Class UrlTester
+ */
+class UrlTester {
+
+    public $results;
+    private $delimiter;
+    private $root_url;
+    private $old_new_urls;
+    private $url_dump;
+
+    /**
+     * @param $root_url
+     * @param $url_dump
+     * @param string $delimiter
+     */
+    function __construct($root_url, $url_dump, $delimiter = ' ') {
+        $this->delimiter = $delimiter;
+        $this->global_url = $root_url;
+        $this->url_dump = $url_dump;
+
+        $this->ProcessRootUrl();
+        $this->ProcessUrlDump();
+        $this->TestAllUrls();
+    }
+
+    /**
+     *
+     */
+    private function ProcessRootUrl() {
+        $this->root_url = 'http://'.$_POST['url'];
+    }
+
+    /**
+     *
+     */
+    private function ProcessUrlDump() {
+        $urls_to_test = explode("\n", $this->url_dump);
+
+        $this->old_new_urls = array();
+
+        foreach ($urls_to_test as $url) {
+            $tmp = explode(' ', $url);
+
+            $tmp[0] = $this->ProcessUrlPart($tmp[0]);
+            $tmp[1] = $this->ProcessUrlPart($tmp[1]);
+
+            $this->old_new_urls[] = array('old_url' => $tmp[0], 'new_url' => $tmp[1]);
+        }
+    }
+
+    /**
+     * @param $url_part
+     * @return string
+     */
+    private function ProcessUrlPart($url_part) {
+        if ($url_part[0] != '/') {
+            return '/'.$url_part;
+        }
+
+        return $url_part;
+    }
+
+    /**
+     *
+     */
+    private function TestAllUrls() {
+        $this->results = array();
+        $this->results['succeeded'] = array();
+        $this->results['failed'] = array();
+
+        $this->FlushOutput('<ol>');
+
+        foreach ($this->old_new_urls as $url) {
+
+            $this->FlushOutput('<li>Opening connection to... '.$this->root_url.$url['old_url'].'<br />');
+            $test = $this->TestUrl($url['old_url'], $url['new_url']);
+            $this->FlushOutput('<li>Closing connection to... '.$this->root_url.$url['old_url'].'<br />');
+
+            if ($test['success']) {
+                $this->results['succeeded'][] = array($url['old_url'].' '.$url['new_url']);
+            } else {
+                $this->results['failed'][] = array($url['old_url'].' '.$url['new_url'], $test['response']);
+            }
+        }
+
+        $this->FlushOutput('</ol>');
+    }
+
+    /**
+     * @param $old_url
+     * @param $new_url
+     * @return array
+     */
+    private function TestUrl($old_url, $new_url) {
+
+        $ch = curl_init($this->root_url.$old_url);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_NOBODY, true);
+        curl_setopt($ch, CURLINFO_EFFECTIVE_URL, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+        curl_exec($ch);
+        $response = curl_getinfo($ch);
+        curl_close($ch);
+
+        if ($response['http_code'] == 200) {// && $response['url'] == $root_url.$new_url) {
+            return array('success' => true);
+        } else {
+            return array('success' => false, 'response' => $response);
+        }
+    }
+
+    /**
+     * @param $content_to_flush
+     */
+    private function FlushOutput($content_to_flush) {
+        ob_start();
+        echo $content_to_flush;
+        ob_end_flush();
+    }
+
+} 
--- index.php
+++ index.php
@@ -18,87 +18,21 @@
 </style>
 
 <?php
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
-
-function NewHTTP($root_url, $old_url, $new_url) {
-
-	echo '<li>Opening connection to... '.$root_url.$old_url.'<br />';
-
-	ob_flush();
-	flush();
-
-	$ch = curl_init($root_url.$old_url);
-	curl_setopt($ch, CURLOPT_HEADER, true);
-	curl_setopt($ch, CURLOPT_NOBODY, true);
-	curl_setopt($ch, CURLINFO_EFFECTIVE_URL, true);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 0);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
-	curl_exec($ch);
-	$response = curl_getinfo($ch);
-	curl_close($ch);
-
-	echo 'Closed connection to... '.$root_url.$old_url.'</li>';
-
-	ob_flush();
-	flush();
-
-	if ($response['http_code'] == 200) {// && $response['url'] == $root_url.$new_url) {
-		return array('success' => true);
-	} else {
-		return array('success' => false, 'response' => $response);
-	}
-}
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 if (isset($_POST['htaccess']) && $_POST['htaccess'] != '' &&
     isset($_POST['url']) && $_POST['url'] != ''):
 
-	echo '<div class="output"><ol>';
+    require_once 'UrlTester.php';
+    $test_urls = new UrlTester($_POST['url'], $_POST['htaccess']);
 
-	$root_url = 'http://'.$_POST['url'];
-	$urls_to_test = explode("\n", $_POST['htaccess']);
+    echo '<h1>Succeeded</h1>';
+    var_dump($test_urls->results['succeeded']);
+    echo '<h1>Failed</h1>';
+    var_dump($test_urls->results['failed']);
 
-	$urls = array();
-
-	foreach ($urls_to_test as $url) {
-		$tmp = explode(' ', $url);
-
-		if ($tmp[0][0] != '/') {
-			$tmp[0] = '/'.$tmp[0];
-		}
-
-		if ($tmp[1][0] != '/') {
-			$tmp[1] = '/'.$tmp[1];
-		}
-
-		$urls[] = array('old_url' => $tmp[0], 'new_url' => $tmp[1]);
-	}
-
-	$result = array();
-	$result['succeeded'] = array();
-	$result['failed'] = array();
-
-	foreach ($urls as $url) {
-		$test = NewHTTP($root_url, $url['old_url'], $url['new_url']);
-
-		if ($test['success']) {
-			$result['succeeded'][] = array($url['old_url'].' '.$url['new_url']);
-		} else {
-			$result['failed'][] = array($url['old_url'].' '.$url['new_url'], $test['response']);
-		}
-	}
-
-
-	echo '<h1>Successes</h1>';
-	var_dump($result['succeeded']);
-	echo '<h1>Failures</h1>';
-	var_dump($result['failed']);
-
-	echo '</ol></div>';
 else: ?>
 
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" />
